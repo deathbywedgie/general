04da8ad2f59ee8428f934d4f35ca396070db8f39
Chad Roberts <jcbroberts@gmail.com>
2024-07-03 12:38:03 -0700

    Example: using FrozenDict to make immutable dicts that can be used as dict keys

---
+++ code_samples/python/immutable_dicts_with_FrozenDict.py
@@ -0,0 +1,11 @@
+"""
+Simplistic example: creating immutable dicts that can be used as dict keys
+"""
+
+from frozendict import frozendict
+
+
+fd = frozendict({'a': 1, 'b': 2})
+nested_dict = {fd: 'value'}
+
+print(nested_dict[fd])  # Output: value
